2665c2b81f1ed62af1c0f1d711d4bdef20fe6953
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 18:04:19 -0300

    update: atualiza métodos get e post de news api

--- bin/api/news_api.dart
+++ bin/api/news_api.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../models/news_model.dart';
 import '../services/generic_service.dart';
 
 class NewsApi {
-  final GenericService _service;
+  final GenericService<NewsModel> _service;
 
   NewsApi(this._service);
 
@@ -12,12 +15,19 @@ class NewsApi {
     final router = Router();
 
     router.get('/news', (Request req) {
-      //TODO: _service.findAll();
-      return Response.ok('hello world');
+      final List<NewsModel> news = _service.findAll();
+      final mapNews = news.map((e) => e.toMap()).toList();
+
+      return Response.ok(
+        jsonEncode(mapNews),
+        headers: {'content-type': 'application/json'},
+      );
     });
 
-    router.post('/news', (Request req) {
-      //TODO: _service.save('');
+    router.post('/news', (Request req) async {
+      final body = await req.readAsString();
+      _service.save(NewsModel.fromMap(jsonDecode(body)));
+
       return Response.ok('hello world');
     });
 
