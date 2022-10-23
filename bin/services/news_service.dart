ec37848c2053297ccb8b3718f3eabf9914a4a9f7
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 18:03:51 -0300

    update: implementa métodos de news service

--- bin/services/news_service.dart
+++ bin/services/news_service.dart
@@ -2,27 +2,37 @@ import '../models/news_model.dart';
 import 'generic_service.dart';
 
 class NewsService implements GenericService<NewsModel> {
+  final List<NewsModel> _newsList = [];
   @override
-  bool delete(int value) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  bool delete(int id) {
+    _newsList.removeWhere((element) => element.id == id);
+    return true;
   }
 
   @override
   List<NewsModel> findAll() {
-    // TODO: implement findAll
-    throw UnimplementedError();
+    return _newsList;
   }
 
   @override
   NewsModel findOne(int id) {
-    // TODO: implement findOne
-    throw UnimplementedError();
+    return _newsList.firstWhere((element) => element.id == id);
   }
 
   @override
   bool save(NewsModel value) {
-    // TODO: implement save
-    throw UnimplementedError();
+    final model = _newsList.firstWhere(
+      (element) => element.id == value.id,
+      orElse: () {
+        return value;
+      },
+    );
+    if (_newsList.contains(model)) {
+      final index = _newsList.indexOf(model);
+      _newsList[index] = value;
+    } else {
+      _newsList.add(value);
+    }
+    return true;
   }
 }
