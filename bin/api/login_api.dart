6136e14e9fc108cae37a96df22c6b82c1f6e9c3f
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 18:44:50 -0300

    update: injeta security na api login

--- bin/api/login_api.dart
+++ bin/api/login_api.dart
@@ -1,29 +1,17 @@
-import 'dart:convert';
-
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../infra/security/security_service.dart';
+
 class LoginApi {
+  final SecurityService _securityService;
+  LoginApi(this._securityService);
+
   Handler get handler {
     final router = Router();
 
     router.post('/login', (Request req) async {
-      final result = await req.readAsString();
-      final Map json = jsonDecode(result);
-      final user = json['user'];
-      final password = json['password'];
-
-      if (user == 'admin' && password == '123456') {
-        final response = jsonEncode({
-          'token': 'token123',
-          'user_id': '1',
-        });
-        return Response.ok(
-          response,
-        );
-      } else {
-        return Response.forbidden('Not authorized');
-      }
+      return Response.ok(await _securityService.generateJWT('1'));
     });
 
     return router;
