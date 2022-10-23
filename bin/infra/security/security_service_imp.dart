a2bf5aba527f160c56606c4e037bdbac587e93ce
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 20:36:42 -0300

    add: implementa contrato de security service

--- bin/infra/security/security_service_imp.dart
+++ bin/infra/security/security_service_imp.dart
@@ -1,4 +1,5 @@
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:shelf/shelf.dart';
 
 import '../../utils/enviroment.dart';
 import 'security_service.dart';
@@ -28,4 +29,34 @@ class SecurityServiceImp implements SecurityService<JWT> {
       return null;
     }
   }
+
+  @override
+  Middleware get authorization {
+    return (Handler handler) {
+      return (Request req) async {
+        final authorizationHeader = req.headers['Authorization'];
+        JWT? jwt;
+        if (authorizationHeader != null) {
+          if (authorizationHeader.startsWith("Bearer ")) {
+            final token = authorizationHeader.substring(7);
+            jwt = await validateJWT(token);
+          }
+        }
+        final request = req.change(
+          context: {'jwt': jwt},
+        );
+        return handler(request);
+      };
+    };
+  }
+
+  @override
+  Middleware get verifyJWT => createMiddleware(
+        requestHandler: (Request req) {
+          if (req.context['jwt'] == null) {
+            return Response.forbidden('Not autorized');
+          }
+          return null;
+        },
+      );
 }
