68187d96c3fbc30445885c3f9643dc36e525de5a
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 20:36:16 -0300

    add: cria contrato de security service

--- bin/infra/security/security_service.dart
+++ bin/infra/security/security_service.dart
@@ -1,4 +1,8 @@
+import 'package:shelf/shelf.dart';
+
 abstract class SecurityService<T> {
   Future<String> generateJWT(String userId);
   Future<T?> validateJWT(String token);
+  Middleware get verifyJWT;
+  Middleware get authorization;
 }
