808fa14983290eb4cb5814481d980a8c1f8516e0
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-24 13:03:04 -0300

    update: adiciona injeção de mysql db config

--- bin/infra/dependency_injector/injects.dart
+++ bin/infra/dependency_injector/injects.dart
@@ -3,6 +3,8 @@ import '../../api/news_api.dart';
 import '../../models/news_model.dart';
 import '../../services/generic_service.dart';
 import '../../services/news_service.dart';
+import '../database/db_config.dart';
+import '../database/mysql_config.dart';
 import '../security/security_service.dart';
 import '../security/security_service_imp.dart';
 import 'dependency_injector.dart';
@@ -10,6 +12,8 @@ import 'dependency_injector.dart';
 class Injects {
   static DependencyInjector init() {
     final di = DependencyInjector();
+
+    di.register<DBConfig>(() => MySqlDBConfig());
 
     di.register<SecurityService>(() => SecurityServiceImp());
 
