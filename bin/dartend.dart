04be314ff130771ae976e6abc7ee646fa6dfad85
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-23 23:29:15 -0300

    update main

--- bin/dartend.dart
+++ bin/dartend.dart
@@ -4,16 +4,21 @@ import 'api/login_api.dart';
 import 'api/news_api.dart';
 import 'infra/dartend_server.dart';
 import 'infra/interceptor_middleware.dart';
+import 'infra/security/security_service.dart';
 import 'infra/security/security_service_imp.dart';
 import 'services/news_service.dart';
 import 'utils/enviroment.dart';
 
 void main() async {
   Enviroment.fromFile('.env');
+  final SecurityService securityService = SecurityServiceImp();
   //Adiciona handlers em cascata
   final cascadeHandler = Cascade()
-      .add(LoginApi(SecurityServiceImp()).handler) //
-      .add(NewsApi(NewsService()).handler) //
+      .add(LoginApi(securityService).getHandler()) //
+      .add(NewsApi(NewsService()).getHandler(middlewares: [
+        securityService.authorization,
+        securityService.verifyJWT,
+      ])) //
       .handler;
 
   //Adiciona middlewares
@@ -24,7 +29,7 @@ void main() async {
 
   //Initializa servidor
   await DartendServer.init(
-    address: await Enviroment.get<String>(key: 'ADDRESS'),
+    ip: await Enviroment.get<String>(key: 'ADDRESS'),
     handler: handler,
     port: await Enviroment.get<int>(key: 'PORT'),
   );
